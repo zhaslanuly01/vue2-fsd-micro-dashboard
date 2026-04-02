af019490e39ccab14f0ddc2cea2db0f58bc1a369
zhaslanuly01 <zhaslanuly01@gmail.com>
2026-04-02 23:12:04 +0500

    fix(user): get profile url

--- src/entities/user/api/user.api.ts
+++ src/entities/user/api/user.api.ts
@@ -2,7 +2,7 @@ import type { AxiosPromise } from 'axios'
 import { apiInstance } from '@/shared/api/local'
 import { UserDto } from './user.dto.types'
 
-const BASE_URL = 'profile'
+const BASE_URL = '/profile'
 
 export const getProfile = (): AxiosPromise<UserDto> => {
   return apiInstance.get(BASE_URL)
